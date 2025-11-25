596e46d3ea127d866ec5af5b88de8c53e154d43b
mdsajib50 <mdnurulalamsiddiki9@gmail.com>
2025-11-25 08:46:31 +0600

    update timer duration to 60 seconds and refactor score and hit logic

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function makeBubble() {
   document.getElementById("bubble-bottom").innerHTML = clutter;
 }
 
-const timer=6;
+const timer=60;
 function runTimer() {
   let timeLeft = timer;
   const timerElement = document.getElementById("timer");
@@ -23,7 +23,35 @@ function runTimer() {
 
     }
   }, 1000);
-}
+};
+
+function getNewHit() {
+  const hitElement = document.getElementById("hit");
+  const currentHit = Math.floor(Math.random() * 10);
+  hitElement.textContent = currentHit.toString();
+  
+};
+
+let score = 0;
+function increaseScore() {
+  score+=10;
+  document.getElementById("score").textContent = score.toString();
+};
+
+document.getElementById("bubble-bottom").addEventListener("click", function (event) {
+  if (event.target.classList.contains("bubble")) {
+    const bubbleValue = parseInt(event.target.textContent);
+    const currentHit = parseInt(document.getElementById("hit").textContent);
+    if (bubbleValue === currentHit) {
+      increaseScore();
+      getNewHit();
+      event.target.remove();
+    }
+  }
+});
+
+increaseScore();
+getNewHit();
 
 runTimer();
 
