df47a1ead28b566f0a8ff89bdaaaa6c2d06a3bbe
mdsajib50 <mdnurulalamsiddiki9@gmail.com>
2025-11-25 08:09:30 +0600

    add timer feature

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-let clutter="";
-for(let i=1;i<=100;i++){
+function makeBubble() {
+  let clutter = "";
+  for (let i = 1; i <= 100; i++) {
     clutter += `<div class="bubble">${Math.floor(Math.random() * 10)}</div>`;
+  }
+  document.getElementById("bubble-bottom").innerHTML = clutter;
 }
-document.getElementById("bubble-bottom").innerHTML = clutter;
+
+const timer=6;
+function runTimer() {
+  let timeLeft = timer;
+  const timerElement = document.getElementById("timer");
+  const interval = setInterval(() => {
+    if (timeLeft > 0) {
+      timeLeft--;
+      timerElement.textContent = `${timeLeft}s`;
+      
+      
+      
+    } else {
+      clearInterval(interval);
+      alert("Game Over");
+
+    }
+  }, 1000);
+}
+
+runTimer();
+
+makeBubble();
