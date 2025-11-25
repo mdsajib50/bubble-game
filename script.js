c3a077c7651043c8397ff26862e0723be9bba6e8
mdsajib50 <mdnurulalamsiddiki9@gmail.com>
2025-11-25 07:39:03 +0600

    create bubble and add random number

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+let clutter="";
+for(let i=1;i<=100;i++){
+    clutter += `<div class="bubble">${Math.floor(Math.random() * 10)}</div>`;
+}
+document.getElementById("bubble-bottom").innerHTML = clutter;
